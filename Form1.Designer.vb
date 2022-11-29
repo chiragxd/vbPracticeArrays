c70285b4fbb206e9c42566c3bcc17ac10d498ae3
Chirag Panchal <63766309+chiragxd@users.noreply.github.com>
2022-11-29 14:55:07 -0500

    Problems Regarding Arrays
    Not Much, But Just To Understand Concepts of Arrays.

---
+++ Form1.Designer.vb
@@ -0,0 +1,231 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmvbArrayPractice
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnEx1 = New System.Windows.Forms.Button()
+        Me.btnEx2 = New System.Windows.Forms.Button()
+        Me.btnEx3 = New System.Windows.Forms.Button()
+        Me.btnEx4 = New System.Windows.Forms.Button()
+        Me.btnEx5 = New System.Windows.Forms.Button()
+        Me.btnEx6 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnEx7 = New System.Windows.Forms.Button()
+        Me.btnEx8 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnEx1
+        '
+        Me.btnEx1.Location = New System.Drawing.Point(43, 17)
+        Me.btnEx1.Name = "btnEx1"
+        Me.btnEx1.Size = New System.Drawing.Size(137, 48)
+        Me.btnEx1.TabIndex = 0
+        Me.btnEx1.Text = "Excercise 1"
+        Me.btnEx1.UseVisualStyleBackColor = True
+        '
+        'btnEx2
+        '
+        Me.btnEx2.Location = New System.Drawing.Point(43, 75)
+        Me.btnEx2.Name = "btnEx2"
+        Me.btnEx2.Size = New System.Drawing.Size(137, 48)
+        Me.btnEx2.TabIndex = 1
+        Me.btnEx2.Text = "Excercise 2"
+        Me.btnEx2.UseVisualStyleBackColor = True
+        '
+        'btnEx3
+        '
+        Me.btnEx3.Location = New System.Drawing.Point(43, 129)
+        Me.btnEx3.Name = "btnEx3"
+        Me.btnEx3.Size = New System.Drawing.Size(137, 48)
+        Me.btnEx3.TabIndex = 2
+        Me.btnEx3.Text = "Excercise 3"
+        Me.btnEx3.UseVisualStyleBackColor = True
+        '
+        'btnEx4
+        '
+        Me.btnEx4.Location = New System.Drawing.Point(43, 185)
+        Me.btnEx4.Name = "btnEx4"
+        Me.btnEx4.Size = New System.Drawing.Size(137, 48)
+        Me.btnEx4.TabIndex = 3
+        Me.btnEx4.Text = "Excercise 4"
+        Me.btnEx4.UseVisualStyleBackColor = True
+        '
+        'btnEx5
+        '
+        Me.btnEx5.Location = New System.Drawing.Point(43, 241)
+        Me.btnEx5.Name = "btnEx5"
+        Me.btnEx5.Size = New System.Drawing.Size(137, 48)
+        Me.btnEx5.TabIndex = 4
+        Me.btnEx5.Text = "Excercise 5"
+        Me.btnEx5.UseVisualStyleBackColor = True
+        '
+        'btnEx6
+        '
+        Me.btnEx6.Location = New System.Drawing.Point(43, 297)
+        Me.btnEx6.Name = "btnEx6"
+        Me.btnEx6.Size = New System.Drawing.Size(137, 48)
+        Me.btnEx6.TabIndex = 5
+        Me.btnEx6.Text = "Excercise 6"
+        Me.btnEx6.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(188, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(402, 17)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "output Each Message In Seprate Messagebox One after other."
+        '
+        'btnEx7
+        '
+        Me.btnEx7.Location = New System.Drawing.Point(43, 351)
+        Me.btnEx7.Name = "btnEx7"
+        Me.btnEx7.Size = New System.Drawing.Size(138, 39)
+        Me.btnEx7.TabIndex = 7
+        Me.btnEx7.Text = "Excercise 7"
+        Me.btnEx7.UseVisualStyleBackColor = True
+        '
+        'btnEx8
+        '
+        Me.btnEx8.Location = New System.Drawing.Point(42, 399)
+        Me.btnEx8.Name = "btnEx8"
+        Me.btnEx8.Size = New System.Drawing.Size(138, 39)
+        Me.btnEx8.TabIndex = 8
+        Me.btnEx8.Text = "Excercise 8"
+        Me.btnEx8.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(186, 313)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(210, 17)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Output Largest Number in Array"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(186, 257)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(404, 17)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Calculate addition of numbers Greater then 20 in one message"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(186, 145)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(270, 17)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Addition of all the objects in one message"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(188, 91)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(188, 17)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "output whole array in one op"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(186, 201)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(206, 17)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "Average of all the data into one"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(188, 362)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(210, 17)
+        Me.Label8.TabIndex = 15
+        Me.Label8.Text = "Output smallest number in array"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(188, 410)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(375, 17)
+        Me.Label3.TabIndex = 16
+        Me.Label3.Text = "Replace all the number with number twice bigger then this."
+        '
+        'FrmvbArrayPractice
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnEx8)
+        Me.Controls.Add(Me.btnEx7)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnEx6)
+        Me.Controls.Add(Me.btnEx5)
+        Me.Controls.Add(Me.btnEx4)
+        Me.Controls.Add(Me.btnEx3)
+        Me.Controls.Add(Me.btnEx2)
+        Me.Controls.Add(Me.btnEx1)
+        Me.Name = "FrmvbArrayPractice"
+        Me.Text = "vbArrayPractice"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnEx1 As Button
+    Friend WithEvents btnEx2 As Button
+    Friend WithEvents btnEx3 As Button
+    Friend WithEvents btnEx4 As Button
+    Friend WithEvents btnEx5 As Button
+    Friend WithEvents btnEx6 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnEx7 As Button
+    Friend WithEvents btnEx8 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,97 @@
+﻿Public Class FrmvbArrayPractice
+    Dim tempData As Integer() = {15, 45, 65, 85, 95}
+    Private Sub btnEx1_Click(sender As Object, e As EventArgs) Handles btnEx1.Click
+
+        Dim i As Integer
+        For i = 0 To 4
+            MsgBox(tempData(i))
+
+        Next
+
+    End Sub
+
+    Private Sub btnEx2_Click(sender As Object, e As EventArgs) Handles btnEx2.Click
+
+        Dim i As Integer
+        Dim x As String
+        For i = 0 To 4
+            x = x & tempData(i) & vbNewLine
+        Next
+        MsgBox(x)
+
+    End Sub
+
+    Private Sub btnEx3_Click(sender As Object, e As EventArgs) Handles btnEx3.Click
+        Dim i As Integer
+        Dim x As Integer
+
+        For i = 0 To 4
+            x = x + tempData(i)
+        Next
+
+        MsgBox(x)
+
+    End Sub
+
+    Private Sub btnEx4_Click(sender As Object, e As EventArgs) Handles btnEx4.Click
+        Dim i As Integer
+        Dim x As Integer
+
+        For i = 0 To 4
+            x = x + tempData(i)
+        Next
+
+        MsgBox(x / i)
+    End Sub
+
+    Private Sub btnEx5_Click(sender As Object, e As EventArgs) Handles btnEx5.Click
+        Dim i As Integer
+        Dim x As Integer
+
+
+        For i = 0 To 4
+            If tempData(i) > 20 Then
+                x = x + tempData(i)
+            End If
+        Next
+        MsgBox(x)
+    End Sub
+
+    Private Sub btnEx6_Click(sender As Object, e As EventArgs) Handles btnEx6.Click
+        Dim i As Integer
+        Dim x As Integer = UBound(tempData)
+        Dim dStore As Integer
+
+        For i = 0 To 4
+            If i = x Then
+                dStore = tempData(i)
+            End If
+        Next
+        MsgBox(dStore)
+    End Sub
+
+    Private Sub btnEx7_Click(sender As Object, e As EventArgs) Handles btnEx7.Click
+        Dim i As Integer
+        Dim x As Integer = LBound(tempData)
+        Dim dStore As Integer
+
+        For i = 0 To 4
+            If i = x Then
+                dStore = tempData(i)
+            End If
+        Next
+        MsgBox(dStore)
+    End Sub
+
+    Private Sub btnEx8_Click(sender As Object, e As EventArgs) Handles btnEx8.Click
+        Dim i As Integer
+        Dim x As String
+        For i = 0 To 4
+            x = x & (tempData(i) * 2) & vbNewLine
+        Next
+        MsgBox(x)
+
+    End Sub
+
+
+End Class
